073dc2fe0189c94857daf74a47566c765c4e69fe
cayanat <cayanat.p@gmail.com>
2017-09-12 18:03:46 +0900

    ฟฟฟ

---
+++ CPE200Lab1/CPE200Lab1/RPNCalculaatorEngine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{
+    class RPNCalculatorEngine : CalculatorEngine
+    {
+        public string Process(string str)
+        {
+            string result="0";
+            Stack rpnStack = new Stack();
+            List<string> parts = str.Split(' ').ToList<string>();
+            for (int i = 0; i < parts.Count; i++)
+            {
+                if (isNumber(parts[i]))
+                {
+                    rpnStack.Push(parts[i]);
+                }
+                if (isOperator(parts[i]))
+                {
+                    string Second = rpnStack.Pop().ToString();
+                    string First = rpnStack.Pop().ToString();
+                    result = calculate(parts[i], First, Second, 4);
+                    rpnStack.Push(result);
+                }
+            }
+            return result;
+        }
+    }
+}
